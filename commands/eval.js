e1624304beee7d9c0a50fa523a536b1eb4600f54
Glitch (azizalp52-yilam) <none>
2020-10-16 07:21:50 +0000

    🗼⛹ Checkpoint ./commands/eval.js:21454926/550 ./ayarlar.json:21454926/36

--- commands/eval.js
+++ commands/eval.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const fs = require('fs');
+const ayarlar = require('../ayarlar.json');
 const { stripIndents } = require('common-tags');
 const { Client, Util } = require('discord.js');
 exports.run = async (client, bot, message, args) => {
-if(message.author.id !== "765319680192938005") return;
     try {
       var code = args.join(" ");
       var evaled = eval(code);
@@ -20,7 +20,7 @@ if (Embed.description.length >= 2048)
       Embed.description = Embed.description.substr(0, 2042) + "```...";
     return message.channel.send(Embed)
     } catch (err) {
-      message.channel.send(```${clean(err)}```);
+      message.channel.send("```${err}```");
     }
 function clean(text) {
   if (typeof(text) === "string")
