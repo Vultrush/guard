f20c5d937210892dda75f58664791cd26879ce39
Glitch (azizalp52-yilam) <none>
2020-10-17 16:09:38 +0000

    👧🐄 Checkpoint ./triple.js:21454926/1740

--- triple.js
+++ triple.js
@@ -655,3 +655,33 @@ client.on("message", message => {
     }
   }
 });
+
+client.on("message", async msg => {
+    if(msg.author.bot) return;
+    if(msg.channel.type === "dm") return;
+        
+    let i = await db.fetch(`reklamFiltre_${msg.guild.id}`) 
+          if (i == 'acik') {
+              const reklam = ["discord.app", "discord.gg", "invite","discordapp","discordgg", ".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", ".party", ".rf.gd", ".az",];
+              if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
+                try {
+                  if (!msg.member.hasPermission("MANAGE_GUILD")) {
+                    msg.delete();                   
+                    let embed = new Discord.RichEmbed()
+                    .setColor(0xffa300)
+                    .setFooter('$adis BOT  -|-  ****** engellendi.', client.user.avatarURL)
+                    .setAuthor(msg.guild.owner.user.username, msg.guild.owner.user.avatarURL)
+                    .setDescription("$adis BOT ****** sistemi, " + `***${msg.guild.name}***` + " adlı sunucunuzda ****** yakaladım.")
+                    .addField('Reklamı yapan kişi', 'Kullanıcı: '+ msg.author.tag +'\nID: '+ msg.author.id, true)
+                    .addField('Engellenen mesaj', msg.content, true)
+                    .setTimestamp()                   
+                    msg.guild.owner.user.send(embed)                       
+                    return msg.channel.send(`${msg.author.tag}, ****** Yapmak Yasak Lanet Zenci!`).then(msg => msg.delete(25000));
+                  }             
+                } catch(err) {
+                  console.log(err);
+                }
+              }
+          }
+          if (!i) return;
+  });
